c7cd4589d1ed319806795ed2d7d5beb19ff5d2ba
DishantDrugkar <dishantdrugkar4@gmail.com>
2026-08-30 09:55:08 +0530

    Majority Element

---
+++ src/main/java/org/example/array/leetcode/Leetcode_169_MajorityElement.java
@@ -0,0 +1,24 @@
+package org.example.array.leetcode;
+
+public class Leetcode_169_MajorityElement {
+    public static void main(String[] args) {
+        int nums[] = {2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums));
+    }
+    public static int majorityElement(int nums[]){
+        int candidate = nums[0];
+        int count = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == candidate){
+                count++;
+            }else{
+                count--;
+                if(count == 0){
+                    candidate = nums[i];
+                    count++;
+                }
+            }
+        }
+        return candidate;
+    }
+}
